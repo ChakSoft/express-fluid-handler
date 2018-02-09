17cd2cf0515d8f2760832922e812fa049975218d
Michael Chacaton <mchacaton@chaksoft.eu>
2018-02-09 14:49:30 +0100

    v1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # express-fluid-handler
 Fluid Request Response ExpressJS handler
+
+## Installation
+
+```
+npm install --save express-fluid-handler
+```
+
+## Usage
+
+```javascript
+const Handler = require('express-fluid-handler')
+const { Router } = require('express')
+
+const router = Router()
+router.get('/', Handler((req) => {
+  // Do some logic and return a result.
+}))
+```
+
+## Reference
+
+`Handler(callback, [options])`
+
+| Option | Description
+| ------ | -----------
+| `callback` | Mandatory callback which returns the result to be sent into the response
+| `options` | Optional callbacks applied before or after the main `callback`
+| `options.before(req)` | `Function` called just before the `callback` is called. It takes the request object in first and only argument. This function **MUST** return the request object.
+| `options.after(req, result)` | `Function` called just after the `callback` has been called. It takes the request object and the result of the `callback` as arguments. This function **MUST** return the result.
+
+`Handler.addBefore(callback)`
+
+Adds a global before callback, which is applied before `options.before` callback. You can add as many *before* callbacks as you want, but ensure they all returns the request object.
+
+̀`Handler.addAfter(callback)`
+
+Adds a global after callback, which is applied just before the result is sent into the Express response object. You can add as many *after* callbacks as you want, but ensure they all returns the result.
+
+> Whatever they are before or after callbacks, they are executed in the exact same order as they have been added.
+
+## Examples
+
+You can read the `test/lib.spec.js` test file to check what is possible with this library.
+
+## Contributing
+
+Feel free to fork this project and make it progress by submit PRs.
+
+Ensure the tests are running by launch `npm run test` before submit any PR. A PR where tests are not running will be immediately declined.
+
+## License
+
+This software is licensed under GNU GPL v3.
---
+++ test/lib.spec.js
@@ -0,0 +1,137 @@
+/* eslint-disable no-undef */
+'use strict'
+
+let fakeRequest = {}
+
+let fakeResponse = {
+  send(data) {
+    return data
+  },
+  json(data) {
+    return data
+  }
+}
+
+const Handler = require('../lib')
+
+beforeEach(() => {
+  fakeRequest = {}
+  Handler.flushBefore()
+  Handler.flushAfter()
+})
+
+test('Handling not promise response', async (done) => {
+  const result = await (Handler(() => {
+    return 'test succeed'
+  })(fakeRequest, fakeResponse))
+  expect(result).toBe('test succeed')
+  done()
+})
+
+test('Handling promise response', async (done) => {
+  const result = await (Handler(() => {
+    return Promise.resolve('test succeed')
+  })(fakeRequest, fakeResponse))
+  expect(result).toBe('test succeed')
+  done()
+})
+
+test('Handling before callbacks', async (done) => {
+  const result = await (Handler((req) => {
+    return Promise.resolve(req.beforeData)
+  }, {
+    before : (req) => {
+      req.beforeData = 'test succeed'
+      return req
+    }
+  })(fakeRequest, fakeResponse))
+  expect(result).toBe('test succeed')
+  done()
+})
+
+test('Handling after callback', async (done) => {
+  const result = await (Handler(() => {
+    return Promise.resolve('test succeed')
+  }, {
+    after : (req, result) => {
+      result = `after: ${result}`
+      return result
+    }
+  })(fakeRequest, fakeResponse))
+  expect(result).toBe('after: test succeed')
+  done()
+})
+
+test('Handling coupled callbacks', async (done) => {
+  const result = await (Handler((req) => {
+    return Promise.resolve(`${req.source} : test succeed`)
+  }, {
+    before : (req) => {
+      req.source = '127.0.0.1'
+      req.destination = 'localhost'
+      return req
+    },
+    after : (req, result) => {
+      result = `${req.source} to ${req.destination} : test succeed`
+      return result
+    }
+  })(fakeRequest, fakeResponse))
+  expect(result).toBe('127.0.0.1 to localhost : test succeed')
+  done()
+})
+
+test('Handling global before callbacks', async (done) => {
+  Handler.addBefore((req) => {
+    req.env = 'test'
+    return req
+  })
+  const result = await (Handler((req) => {
+    return Promise.resolve(`Environment : ${req.env}`)
+  })(fakeRequest, fakeResponse))
+  expect(result).toBe('Environment : test')
+  done()
+})
+
+test('Handling global after callbacks', async (done) => {
+  Handler.addAfter((req, result) => {
+    result = {
+      nano : result,
+      env : 'test'
+    }
+    return result
+  })
+  const result = await (Handler(() => {
+    return Promise.resolve('test succeed')
+  })(fakeRequest, fakeResponse))
+  expect(result.nano).toBe('test succeed')
+  expect(result.env).toBe('test')
+  done()
+})
+
+test('Handling all callbacks', async (done) => {
+  Handler.addBefore((req) => {
+    req.env = 'test'
+    return req
+  })
+  Handler.addAfter((req, result) => {
+    return {
+      nano : result,
+      env : req.env,
+    }
+  })
+  const result = await (Handler(() => {
+    return Promise.resolve('test succeed')
+  }, {
+    before : (req) => {
+      req.destination = 'localhost'
+      return req
+    },
+    after : (req, result) => {
+      result = `Destination: ${req.destination}; Message: ${result}`
+      return result
+    }
+  })(fakeRequest, fakeResponse))
+  expect(result.nano).toBe('Destination: localhost; Message: test succeed')
+  expect(result.env).toBe('test')
+  done()
+})
